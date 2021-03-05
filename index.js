af6758e7d2fb4a66f76acbfb80ac0dbc1d694302
tadzyla <tadas.zvinklys@gmail.com>
2021-03-05 17:23:37 +0000

    adding files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+      app = express(),
+      mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb//localhost/to-do', (err) => {
+    err && console.error(err);
+})
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render('blank', {
+        title: 'Mastering Flux',
+        body: 'Hello World 1'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('We are running a server');
+})
+
+
